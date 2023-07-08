4a33d25eab1c62bfcf705fef5e8b5f6850a1b035
Harry <harshitscience30@gmail.com>
2023-07-08 16:02:41 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use(bodyParser.json({limit:'30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit:'30mb', extended: true}));
+app.use(cors());
+app.use('/posts',postRoutes);
+
+
+const CONNECTION_URL= "mongodb+srv://harshit3011:coolboi@cluster0.pscohpi.mongodb.net/?retryWrites=true&w=majority";
+
+const port = process.env.port || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+  .then(()=> app.listen(port, ()=>console.log(`Server Running on port ${port}`)))
+  .catch((error)=>console.log(error.message))
+
+// mongoose.set('useFindAndModify', false);
